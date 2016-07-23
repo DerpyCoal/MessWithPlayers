f1c97f74d9955196348b28cd010b2dc66ca2fe6b
Doctor Internet <jonjon1234.github@gmail.com>
2016-07-23 10:19:44 +0100

    Added the AddCommand function
    Added a function to create a concommand, with the option for advanced
    permissions handling. I might want to add CAMI support at some point.

--- lua/autorun/server/mwp.lua
+++ lua/autorun/server/mwp.lua
@@ -3,8 +3,20 @@ The Official MessWithPlayers script by Lavacoal123
 With help from meharryp and John Internet.
 Deviations of this script must be posted on GitHub (http://www.github.com)
 Copyright - Creative Commons Attribution Non-Commercial License
- ]]--
+]]--
 
+--[[ Structure: Command Data
+required
+callback: function(ply, cmd, args) - The function that is run if the user passes validity checks
+
+optional
+rank: string for the rank. (user (true), admin (IsAdmin), superadmin (IsSuperAdmin) or other (IsUserGroup) - If blank then assumes admin, unless accessFunc is filled.
+accessFunc: function(ply) a function that is called to check if a user has access to a command. return true to allow access, false to deny, blank to try rank, if data.rank is not null.
+
+]]--
+
+-- This function takes a player's name as an input, returning the player data if found.
+-- FindPlayer(string), returns Player
 local function FindPlayer( ply )
 	for k,v in pairs( player.GetAll() ) do
 		if string.find( v:Name():lower(), ply:lower() ) then
@@ -12,6 +24,49 @@ local function FindPlayer( ply )
 		end
 	end
 end
+
+-- This function takes the name and data for a command, and creates it.
+-- AddCommand(string, table), returns Nil
+local function AddCommand( name, data )
+	-- Start by checking the validity of the data passed to the function.
+	-- Assert in Lua, is the same as "if not argument[1] then error(argument[2]) end"
+	-- So in this case, if not name then error("Name not passed...") end
+	assert(name, "Name not passed to AddCommand")
+	assert(data, "Data not passed to AddCommand")
+	if not data:istable() then error("Data is not a table.") end
+	
+	data.internalAccess = nil
+	
+	if data.rank and not data.accessFunc then
+		if data.rank == "user" then
+			data.rankFunc = function(ply) return true end
+		elseif data.rank == "admin" then
+			data.rankFunc = function(ply) return ply:IsAdmin() or ply:IsSuperAdmin() end
+		elseif data.rank == "superadmin" then
+			data.rankFunc = function(ply) return ply:IsSuperAdmin() end
+		else
+			data.rankFunc = function(ply) return ply:IsUserGroup(data.rank) end
+		end
+		
+		data.internalAccess = data.rankFunc
+	elseif not data.rank and data.accessFunc then
+		data.internalAccess = data.accessFunc
+	elseif data.rank and data.accessFunc then
+		data.internalAccess = function(ply) if not data.accessFunc(ply) == nil then return data.accessFunc(ply) else return data.rankFunc(ply) end end
+	else
+		data.internalAccess = function(ply) return ply:IsAdmin() end
+	end
+	
+	concommand.Add(name, function(ply, cmd, args)
+		if ply:IsValid() then
+			if data.internalAccess(ply) then
+				callback(ply, cmd, args)
+			end
+		end
+	end)
+end
+	
+	
 
 concommand.Add( "burnplayer", function( ply, cmd, args )
 	if ply:IsAdmin() then
